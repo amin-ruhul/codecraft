eec4c8b18607d99956c68d52f6e484cc3f425099
Ruhul <ruhulinfo50@gmail.com>
2024-11-04 13:05:34 +0600

    Add edotor seting store

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateRandomName() {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const nameLength = Math.floor(Math.random() * 3) + 3;
+  let name = "";
+
+  for (let i = 0; i < nameLength; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    name += letters[randomIndex];
+  }
+
+  return name.charAt(0).toUpperCase() + name.slice(1);
 }
